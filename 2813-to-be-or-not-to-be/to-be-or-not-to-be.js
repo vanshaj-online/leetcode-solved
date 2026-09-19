5c94a0493e4e73e2fc1446ca2ed41a0e3a248a6b
vanshaj <singhvanshaj09@gmail.com>
2026-09-19 17:25:41 +0530

    Time: 43 ms (60.02%) | Memory: 52.5 MB (93.18%) - LeetSync

---
+++ 2813-to-be-or-not-to-be/to-be-or-not-to-be.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function (val) {
+    const toBe = (other) => {
+        if (other === val) return true
+        throw new Error("Not Equal")
+    }
+    const notToBe = (other) => {
+        if (other !== val) return true
+        throw new Error("Equal")
+    }
+    return { toBe, notToBe }
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
